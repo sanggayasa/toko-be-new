c32cc2b92fd37df1bd76e5da03df942a59ff7a4b
sanggayasa <sanggayasa502@gmail.com>
2024-07-22 19:03:39 +0700

    update bugs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+    rawBody: true,
+  });
+  // App Prefix
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+  });
+
+  // App Body Parser
+  app.useBodyParser('urlencoded', { extended: true });
+  app.disable('x-powered-by');
+  app.use(helmet());
   app.enableCors({
     origin: ['*', 'https://toko-peach.vercel.app','https://vercel.app','toko-peach.vercel.app', 'vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
