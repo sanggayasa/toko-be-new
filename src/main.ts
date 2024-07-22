08605b61e717d79a40d3695a210994692d70e272
sanggayasa <sanggayasa502@gmail.com>
2024-07-22 18:30:32 +0700

    update bugs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      '*',
+      'http://localhost:3001',
+      'http://localhost:3000',
+      'http://localhost',
+      'https://toko-peach.vercel.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
