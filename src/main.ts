fac838203f1794d1b100593586adfb84f64c22bf
sanggayasa <sanggayasa502@gmail.com>
2024-07-23 10:17:31 +0700

    update bugs

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   // app.disable('x-powered-by');
   // app.use(helmet());
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept',
-    credentials: true,
-    preflightContinue: false,
-  });
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   allowedHeaders: 'Content-Type, Accept',
+  //   credentials: true,
+  //   preflightContinue: false,
+  // });
   await app.listen(3000);
 }
 bootstrap();
