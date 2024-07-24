7a708c10ba2f286becb193d91501a00bb5491bff
sanggayasa <sanggayasa502@gmail.com>
2024-07-24 10:21:53 +0700

    tambah feature chat

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -4,23 +4,23 @@ import { ProductController } from './product.controller';
 import { Product } from './entities/product.entity';
 // import { ListProduct } from './entities/list-product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { MulterModule } from '@nestjs/platform-express';
-// import { diskStorage } from 'multer';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 // import { ProductImage } from 'src/product-image/entities/product-image.entity';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Product]),
     // TypeOrmModule.forFeature([ListProduct]),
-    // MulterModule.register({
-    //   storage: diskStorage({
-    //     destination: './uploads', // Adjust the directory path for uploads
-    //     filename: function (req, file, cb) {
-    //       // const uniqueSuffix =
-    //       //   Date.now() + '-' + Math.round(Math.random() * 1e9);
-    //       cb(null, file.fieldname + '-' + file.originalname);
-    //     },
-    //   }),
-    // }),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './uploads', // Adjust the directory path for uploads
+        filename: function (req, file, cb) {
+          // const uniqueSuffix =
+          //   Date.now() + '-' + Math.round(Math.random() * 1e9);
+          cb(null, file.fieldname + '-' + file.originalname);
+        },
+      }),
+    }),
   ],
   providers: [ProductService],
   controllers: [ProductController],
